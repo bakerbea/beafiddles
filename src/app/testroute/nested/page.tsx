f75c311aa522cf7568b079c9c47f01aedd566c29
Danielle Siy <beasiy@MacBook-Pro.local>
2023-02-26 01:33:35 +0800

    Tested Material UI and route creation

---
+++ src/app/testroute/nested/page.tsx
@@ -0,0 +1,9 @@
+function NestedTest() {
+    return (
+        <div>
+            <p>This page tests adding a nested route via folder in /app</p>
+        </div>
+    )
+}
+
+export default NestedTest
---
+++ src/app/testroute/page.tsx
@@ -0,0 +1,10 @@
+function Test() {
+    return (
+        <div>
+            <p>This page tests adding a route via folder in /app</p>
+            <a href="/testroute/nested">This tests folder nested routes</a>
+        </div>
+    )
+}
+
+export default Test
